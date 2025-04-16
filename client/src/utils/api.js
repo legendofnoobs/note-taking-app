a26f8d147a69be436a5ff37767629ec1ce390f23
legendofnoobs <154618805+legendofnoobs@users.noreply.github.com>
2025-04-16 23:38:58 +0200

    env add

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+const baseURL = import.meta.env.VITE_BASE_URL
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: baseURL,
     headers: {
         'Content-Type': 'application/json',
     },
