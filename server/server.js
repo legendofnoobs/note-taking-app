25204a4514e7c0786791aeaa57e0e966cd2172cb
legendofnoobs <154618805+legendofnoobs@users.noreply.github.com>
2025-04-17 00:14:30 +0200

    serverless

--- server/server.js
+++ server/server.js
@@ -1,32 +1,30 @@
 import express from "express";
 import dotenv from "dotenv";
-import { connectDB } from "./database/connection.js";
 import cors from "cors";
+import serverless from "serverless-http";
+import { connectDB } from "../database/connection.js";
 
-import authRouter from "./routes/authRoute.js";
-import noteRouter from "./routes/noteRoute.js";
+import authRouter from "../routes/authRoute.js";
+import noteRouter from "../routes/noteRoute.js";
 
-// https://note-taking-app-five-wine.vercel.app
-// http://localhost:5173
-
-const app = express();
 dotenv.config();
+const app = express();
+
 app.use(cors({
     origin: 'https://note-taking-app-five-wine.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
 app.use(express.json());
 connectDB();
 
-app.listen(5000, () => {
-    console.log("Server is running on port 5000");
-});
-
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.send("Hello World from Vercel");
 });
 
 app.use("/api/auth", authRouter);
 app.use("/api/notes", noteRouter);
+
+export const handler = serverless(app);
